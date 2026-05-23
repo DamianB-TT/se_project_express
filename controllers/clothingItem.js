73163146d88d888f0cdd6208d76e2a4aa5a55301
DamianB <damian.byrd00@gmail.com>
2026-05-22 21:10:14 -0700

    Initialize Express/MongoDB backend with user and clothing item models, controllers, routes, and validation

---
+++ controllers/clothingItem.js
@@ -0,0 +1,48 @@
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  const { name, weather, imageURL } = req.body;
+
+  ClothingItem.create({ name, weather, iamgeURL })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.param;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.param;
+
+  console.log(itemId);
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((err) => {
+      res.status(500).send({ message: err });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
---
+++ models/clothingItem.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
