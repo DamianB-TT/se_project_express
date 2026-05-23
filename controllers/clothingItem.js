c5f57126c35456b23e544d99f6c2dc3ba6850344
DamianB <damian.byrd00@gmail.com>
2026-05-22 21:31:37 -0700

    Get help on mistakes and completely refactor everything

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,48 +1,119 @@
 const ClothingItem = require("../models/clothingItem");
 
 const createItem = (req, res) => {
-  const { name, weather, imageURL } = req.body;
+  const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({ name, weather, imageURL })
-    .then((item) => {
-      console.log(item);
-      res.send({ data: item });
-    })
+  ClothingItem.create({ name, weather, imageUrl })
+    .then((item) => res.status(201).send(item))
     .catch((err) => {
-      res.status(500).send({ message: err });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+
+      return res.status(500).send({ message: err.message });
     });
 };
 
 const getItems = (req, res) => {
   ClothingItem.find({})
     .then((items) => res.status(200).send(items))
-    .catch((err) => {
-      res.status(500).send({ message: err });
-    });
+    .catch((err) => res.status(500).send({ message: err.message }));
 };
 
 const updateItem = (req, res) => {
-  const { itemId } = req.param;
-  const { imageURL } = req.body;
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
 
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+  ClothingItem.findByIdAndUpdate(
+    itemId,
+    { $set: { imageUrl } },
+    { new: true, runValidators: true }
+  )
     .orFail()
     .then((item) => res.status(200).send(item))
     .catch((err) => {
-      res.status(500).send({ message: err });
+      if (err.name === "ValidationError") {
+        return res.status(400).send({ message: err.message });
+      }
+
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item id" });
+      }
+
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Item not found" });
+      }
+
+      return res.status(500).send({ message: err.message });
     });
 };
 
 const deleteItem = (req, res) => {
-  const { itemId } = req.param;
+  const { itemId } = req.params;
 
-  console.log(itemId);
   ClothingItem.findByIdAndDelete(itemId)
     .orFail()
-    .then((item) => res.status(204).send(item))
+    .then((item) => res.status(200).send(item))
     .catch((err) => {
-      res.status(500).send({ message: err });
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item id" });
+      }
+
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Item not found" });
+      }
+
+      return res.status(500).send({ message: err.message });
     });
 };
 
-module.exports = { createItem, getItems, updateItem, deleteItem };
+const likeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item id" });
+      }
+
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Item not found" });
+      }
+
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const dislikeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(200).send(item))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Invalid item id" });
+      }
+
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Item not found" });
+      }
+
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+};
